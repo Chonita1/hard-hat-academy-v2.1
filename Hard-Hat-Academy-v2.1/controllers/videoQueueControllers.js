f348d1564f559410817cb5f4381921a0751a0849
Chonita1 <chonita@starpower.net>
2021-12-16 13:44:19 -0500

    completed code addvideo delete from queue

--- Hard-Hat-Academy-v2.1/controllers/videoQueueControllers.js
+++ Hard-Hat-Academy-v2.1/controllers/videoQueueControllers.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const videoQueue = require('../models/videoQueue')
+const VideoQueue = require('../models/videoQueue')
 
-router.put('/users/addvideo', (req, res) => {
+router.put('/users/addvideo/:name/:videourl', (req, res) => {
     if(req.session.currentUser) {
         try{
             Users.findOneAndUpdate({username: req.session.currentUser.username},
             {                    
                 $push: {
                     queue: {
-                        name: "Welding",
-                        videoUrl: "https://www.youtube.com/embed/qggvdb8_xB8",
-    
+                        name: req.params.name,
+                        videoUrl: req.params.videourl,
+                        hasWatched: false    
                     }
                 }
             },
@@ -30,9 +30,86 @@ router.put('/users/addvideo', (req, res) => {
 })
 
 router.get('/users/showqueue', (req, res) => {
-
+    if(req.session.currentUser) {
+        try{
+            VideoQueue.findOne({username: req.session.currentUser.username}, (err, allVideos) => {
+            // ? is equal to if error then output error msg
+            err ? res.send(err)
+            // : is equal to else...
+            : res.render('videoQueue.ejs', {
+                videoArray: allVideos
+            })
+        })
+        }
+        catch (err) {
+            res.send(err.message)
+        }
+    } else {
+        res.redirect('/')
+    }
 })
 
 router.put('/users/deletevideo', (req, res) => {
+        if(req.session.currentUser) {
+            try{
+                Users.findOneAndUpdate({username: req.session.currentUser.username},
+                {   
+                    
+                // exports.destroyLink = function(req, res) {
+                // Node.findByIdAndUpdate(
+                //     req.params.id, { $pull: { "configuration.links": { _id: req.params.linkId } } }, { safe: true, upsert: true },
+                //     function(err, node) {
+                //         if (err) { return handleError(res, err); }
+                //         return res.status(200).json(node.configuration.links);
+                //     });
+                // };
+                 
+                    $pull: {
+                        queue: {
+                            name: req.params.name,
+                            videoUrl: req.params.videourl,  
+                        }
+                    }
+                },
+                {safe:true, new:true},
+                (err, userQueue) => {
+                    err ? res.send(err)
+                    : res.redirect('/')
+                })
+            }
+            catch (err) {
+                res.send(err.message)
+            }
+        } else {
+            res.redirect('/')
+        }
+    })
+    
 
-})
+
+
+
+        // Added 12/16
+//     Users.findByIdAndDelete({username: req.session.currentUser.username},
+//         {                    
+//             $push: {
+//                 queue: {
+//                     name: "Welding",
+//                     videoUrl: "https://www.youtube.com/embed/qggvdb8_xB8",
+
+//                 }
+//             }
+//         },
+//             {new:true},
+//             (err, userQueue) => {
+//                 err ? res.send(err)
+//                 : res.redirect('/')
+//             })
+//     }
+//     catch (err) {
+//         res.send(err.message)
+//     }
+//     } else {
+//         res.redirect('/')
+//     }
+// })
